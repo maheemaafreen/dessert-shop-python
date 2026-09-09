cde55567eda0ee088af3f1578e3e8b26dbaf692e
maheemaafreen <afreenmaheema@gmail.com>
2026-09-09 01:09:59 +0100

    Refactoring the system
    Exercising the concept of functions to clean up code

--- dessert_shop.py
+++ dessert_shop.py
@@ -3,29 +3,45 @@ prices = {"ice cream":2, "chocolate":1, "brownies": 3, "cookies" : 2, "cake" :5}
 again = "yes"
 grand_total = 0
 orders = []
-name = input("Hi, what's your name? :")
+name = input("Hi, what's your name? : ")
+
+# FUNCTIONS
+def calc_total(price, quan):
+    return price * quan
 def greet(name):
     print(f"Hello, {name}! Welcome to the dessert shop!")
-greet(name)
-while again == "yes":
-    order = input("What dessert would you like?")
-    if order in foods:
-        print("Perfect, will do!")
-        quan = int(input("How many would you like? : "))
-        while quan <= 0:
-            quan = int(input("Sorry, that's not a valid quantity! What quantity would you like? : "))
-        orders.append([order, quan])
-        price = prices[order]
-        total = price * quan
-        grand_total += total
-        print("Coming up! Your total is £", total)
-    else:
-        print("Sorry! We don't have that!")
-    again = input("Would you like to place another order? (yes/no): ")
+def get_quantity():
+    quan = int(input("How many would you like? : "))
+    while quan <= 0:
+        quan = int(input("Sorry, that's not a valid quantity! What quantity would you like? : "))
+    return quan
+def get_price(order):
+    return prices[order]
 def receipt():
     print("Thanks for buying! Here's your receipt:")
     print("~YOUR ORDER~")
     for order in orders:
         print(f"{order[1]} x {order[0]}")
     print(f"GRAND TOTAL: £{grand_total}")
+def place_order():
+    order = input("What dessert would you like?")
+    if order in foods:
+        print("Perfect, will do!")
+        quan = get_quantity()
+        orders.append([order, quan])
+        price = get_price(order)
+        total = calc_total(price, quan)
+        print("Coming up! Your total is £", total)
+        return total
+    else:
+        print("Sorry! We don't have that!")
+        return 0
+#
+
+greet(name)
+while again == "yes":
+    total = place_order()
+    grand_total += total
+    again = input("Would you like to place another order? (yes/no): ")
+
 receipt()
